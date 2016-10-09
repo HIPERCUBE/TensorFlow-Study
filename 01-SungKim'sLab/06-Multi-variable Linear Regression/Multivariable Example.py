e5cf85bd689ed2dc0cf8b3c2a79840c4401b0e8a
Joowon Ryoo <joowonryoo@gmail.com>
2016-10-09 23:28:31 +0900

    Add Multivariable Regression Example

---
+++ 01-SungKim'sLab/06-Multi-variable Linear Regression/Multivariable Example.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+# data
+x1_data = [1, 0, 3, 0, 5]
+x2_data = [0, 2, 0, 4, 0]
+y_data = [1, 2, 3, 4, 5]
+
+W1 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+W2 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
+b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
+# Our hypothesis
+hypothesis = W1 * x1_data + W2 * x2_data + b
+
+# Simplified cost function
+cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+
+# Minimize
+a = tf.Variable(0.1)  # Learning rate, alpha
+optimizer = tf.train.GradientDescentOptimizer(a)
+train = optimizer.minimize(cost)
+
+# Before starting, initialize the variables. We will 'run' this first.
+init = tf.initialize_all_variables()
+
+# Launch the graph.
+sess = tf.Session()
+sess.run(init)
+
+# Fit the line.
+for step in xrange(2001):
+    sess.run(train)
+    if step % 20 == 0:
+        print step, sess.run(cost), sess.run(W1), sess.run(W2), sess.run(b)
